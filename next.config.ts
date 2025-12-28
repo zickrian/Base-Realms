bcf0917e13ec9ec6b6d85423661bc083559ca9e7
FirdausKz <espejodaniel50@gmail.com>
2025-12-28 23:29:57 +0700

    feat: add remote image configuration for Supabase storage in Next.js

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'htdiytcpgyawxzpitlll.supabase.co',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
+  },
   webpack: (config) => {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
