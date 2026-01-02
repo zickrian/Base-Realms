5a969028193870f416fb783aed8c18a8ca0f289c
lafatha <athallahfatha@gmail.com>
2026-01-02 15:58:41 +0700

    remove x componen

--- app/components/game/CardRevealModal.tsx
+++ app/components/game/CardRevealModal.tsx
@@ -338,18 +338,7 @@ export function CardRevealModal({
               </div>
             )}
             
-            {/* Close button after flip - hide during rotation/flipping */}
-            {revealState === "card1" && !isAnimatingToInventory && !isRotating && (
-              <button 
-                className={styles.closeButton}
-                onClick={handleCloseClick}
-                aria-label="Close"
-              >
-                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M15 5L5 15M5 5L15 15" stroke="currentColor" strokeWidth="2" strokeLinecap="round"/>
-                </svg>
-              </button>
-            )}
+
           </>
         )}
       </div>
